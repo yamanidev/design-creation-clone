1387cc2bc6134af5368c006713e261f4c5dc506d
yamanidev <elamine.yamani@gmail.com>
2022-10-21 22:02:14 +0100

    Added custom cursor

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,8 +1,15 @@
 const mainMenu = document.getElementById("main-menu");
 const mainNav = document.getElementsByClassName("main-nav")[0];
 const burger = document.getElementById("burger");
+const cursor = document.getElementById("cursor");
 
 burger.addEventListener("click", () => {
   mainMenu.classList.toggle("menu-clicked");
   mainNav.classList.toggle("menu-clicked");
+  cursor.classList.toggle("menu-clicked");
+});
+
+document.addEventListener("mousemove", (event) => {
+  cursor.style.top = `${event.pageY - 12.5}px`;
+  cursor.style.left = `${event.pageX - 12.5}px`;
 });
